8d9e7ddd76cf6c0aa5758febc7a1f13880d4c4ef
Lorenzo De Bie <Lorenzo.DeBie@fednot.be>
2023-08-20 14:54:54 +0200

    Day 1 Challenge

--- 2022/go/day01/main.go
+++ 2022/go/day01/main.go
@@ -1,21 +1,37 @@
 package day01
 
 import (
+	"sort"
 	"strconv"
 	"strings"
 )
 
 type Day01 struct {
-	elves [][]int
+	elves  [][]int
+	totals []int
 }
 
 // Solve part 1
 func (d *Day01) Part1() {
-
+	result := 0
+	for _, elf := range d.elves {
+		sum := 0
+		for _, snack := range elf {
+			sum += snack
+		}
+		if sum > result {
+			result = sum
+		}
+		d.totals = append(d.totals, sum)
+	}
+	println(result)
 }
 
 // Solve part 2
 func (d *Day01) Part2() {
+	sort.Ints(d.totals)
+	sort.Sort(sort.Reverse(sort.IntSlice(d.totals)))
+	println(d.totals[0] + d.totals[1] + d.totals[2])
 
 }
 
