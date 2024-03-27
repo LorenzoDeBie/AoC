3278f59ce896a7f76846d7a4fa57b96164799786
Lorenzo De Bie <Lorenzo.DeBie@fednot.be>
2024-03-28 00:34:47 +0100

    Day 01 - Part 01

--- 2023/rust/days/day-01/src/part1.rs
+++ 2023/rust/days/day-01/src/part1.rs
@@ -1,10 +1,29 @@
 use crate::custom_error::AocError;
-
+use tracing::{debug, log::info};
 #[tracing::instrument]
 pub fn process(
     _input: &str,
 ) -> miette::Result<String, AocError> {
-    todo!("day-01 - part 1");
+    info!("Solving Day1, part 1");
+    let result = _input
+        // split into lines
+        .lines()
+        // map each line to digit
+        .map(|line| {
+            debug!("Analyzing line {line}");
+            // get first digit
+            let first = line.chars().find_map(|c| {
+                c.to_digit(10)
+            }).expect("Should be a number!");
+            // get last digit
+            let last = line.chars().rev().find_map(|c| {
+                c.to_digit(10)
+            }).expect("Should be a number!");
+            // return concatenated digit
+            first * 10 + last
+            // sum all the digits
+        }).sum::<u32>();
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -13,9 +32,11 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet";
+        assert_eq!("142", process(input)?);
         Ok(())
     }
 }
