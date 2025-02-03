0dfc37da4040a1fcc7479d84d6c3f6fddf4946c9
Lorenzo De Bie <lorenzo.de.bie@VANHEEDE.COM>
2025-02-03 21:46:10 +0100

    2024 rust day 04 part 2

--- 2024/rust/days/day-04/src/part2.rs
+++ 2024/rust/days/day-04/src/part2.rs
@@ -1,10 +1,58 @@
+use std::collections::HashMap;
+use glam::IVec2;
 use crate::custom_error::AocError;
 
 #[tracing::instrument]
 pub fn process(
     _input: &str,
 ) -> miette::Result<String, AocError> {
-    todo!("day-04 - part 2");
+
+    // Vecs for each M and S we can find from an A position
+    const DIRECTIONS: [[IVec2; 2]; 4] = [
+        [IVec2::new(1, 1), IVec2::new(-1, -1)],
+        [IVec2::new(-1, -1), IVec2::new(1, 1)],
+        [IVec2::new(1, -1), IVec2::new(-1, 1)],
+        [IVec2::new(-1, 1), IVec2::new(1, -1)],
+    ];
+
+    const SEQUENCE: [char; 2] = ['M', 'S'];
+
+    // Construct grid in a Hashmap with IVec2 as keys and the char as value
+    let grid = _input.lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars().enumerate().map(move |(x, c)| {
+                (IVec2::new(x as i32, y as i32), c)
+            })
+        }).collect::<HashMap<_, _>>();
+
+
+    let result : usize = grid.iter()
+        // only concerned with A as start position
+        .filter(|(_pos, c)| { **c == 'A' })
+        .filter(|(pos, _c)| {
+            // for each A we iter over all possible directions
+            DIRECTIONS.iter()
+                .map(|mas_positions| {
+                    mas_positions.iter()
+                        .map(|mas_offset| {
+                            // Get the character at the offset
+                            grid.get(&(**pos + *mas_offset))
+                        })
+                        .enumerate()
+                        .all(|(idx, c)| {
+                            // check if char matches with M or S at offset
+                            SEQUENCE.get(idx) == c
+                        })
+                })
+                .filter(|b| *b)
+                .count()
+                // Now we need to have exactly 2 matches instead of just counting them
+                == 2
+        })
+        .count();
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -13,9 +61,17 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
+        assert_eq!("9", process(input)?);
         Ok(())
     }
 }
